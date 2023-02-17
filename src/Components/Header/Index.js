381cde4c3b5f0581d6861e85aa324c810f202b27
fouad ajayi <fouadajayi6@gmail.com>
2023-02-17 17:43:32 +0100

    first commit

---
+++ src/Components/Header/Index.js
@@ -0,0 +1,36 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./style.css";
+import logo from "../Assets/Artboard 1 5.png"
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+	};
+
+	return (
+		<header>
+			<img src={logo} alt=""></img>
+			
+			<nav ref={navRef}>
+				<a href="/#" className="first">Home</a>
+				<a href="/#">Blog</a>
+				<a href="/#">About Us</a>
+				<a href="/#">Pricing</a>
+
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button className="nav-btn" onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
