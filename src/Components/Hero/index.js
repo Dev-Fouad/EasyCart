c61419009194d9d5a58e02962ec15142e23d84fd
fouad ajayi <fouadajayi6@gmail.com>
2023-02-18 20:53:22 +0100

    SRC

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -7,7 +7,7 @@ import "./style.css"
 
 const Hero = () => {
   return (
-    <div className='justify-center h-[90rem] flex flex-col items-center '>
+    <div className='border-2   h-[69rem] flex flex-col items-center  pt-[8rem]'>
       
       <div className='border-2 w-[305px] rounded-[16px] py-[3px] px-[2px] bg-[#FFF4ED] flex space-x-[12px] font-inter  mr-[3rem]'>
         <p className=' font-medium leading-5 text-[14px] bg-[#EF6820] w-[55px] text-[#FFFFFF] h-[24px] rounded-[16px] text-center pt-[2px]'>New!</p>
@@ -26,11 +26,11 @@ const Hero = () => {
       <div className='Back relative'>
         <img src={Appss} alt="" ></img>
 
-        <div className=' max-w-[345px] h-[125px] bg-[#ffff] p-[10px] absolute bottom-[23px]'>
-          <p className='text-[15px] leading-[17px] font-inter'>I've become more conscious of how I spend my money and also about investing. Now, I feel comfortable spending knowing my Cowrywise account is there.</p>
+        <div className=' w-[93%] h-[120px] bg-[#ffff] py-[12px]  px-[14px] absolute bottom-[23px]'>
+          <p className='text-[16px] leading-[17px]'>I've become more conscious of how I spend my money and also about investing. Now, I feel comfortable spending knowing my Cowrywise account is there.</p>
           <div className='flex space-x-[10px]  pt-[5px]'>
-            <h2 className=' font-semibold text-[#344054] flex'>C.E.O Ankara Stores</h2>
-            <h6 className=' flex  font-semibold text-[#667085] text-[14px] leading-[16px]'>Folake Akinoso</h6>
+            <h2 className=' font-bold text-[#344054] flex'>C.E.O Ankara Stores</h2>
+            {/* <h6 className=' flex  font-semibold text-[#667085] text-[14px] '>Folake Akinoso</h6> */}
           </div>
         </div>
       
